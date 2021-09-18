3293bf96170a3b1fe7091f1aa700e65f326f1246
Ken Chen <chengusky@gmail.com>
2021-09-18 09:50:04 +0800

    Fix exclusiveMinimum and exclusiveMaximum data type in open-api

--- test/open-api.spec.js
+++ test/open-api.spec.js
@@ -1,3 +1,6 @@
+const Ajv = require('ajv-draft-04')
 const executeTests = require('./base')
 
-executeTests('open-api')
+const ajv = new Ajv()
+ajv.addMetaSchema(require('../schemas/openapi-3.0.json'))
+executeTests('open-api', ajv)
