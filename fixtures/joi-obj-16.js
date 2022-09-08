3e713f2377f1cefb8abf28f202a9a430237d34c0
Jan Dockx <Jan_Dockx@peopleware.be>
2022-09-08 12:58:19 +0200

    add read- and writeOnly properties to fixtures and outputs
    except for openapi

--- fixtures/joi-obj-16.js
+++ fixtures/joi-obj-16.js
@@ -87,5 +87,9 @@ module.exports = joi.object().keys({
     }).unknown(false))
     .description('Some kind of list')
     .optional(),
-  children: joi.array().items(joi.link('#person'))
+  children: joi.array().items(joi.link('#person')),
+  readOnlyTrue: joi.string().meta({ readOnly: true }),
+  readOnlyFalse: joi.string().meta({ readOnly: false }),
+  writeOnlyTrue: joi.string().meta({ writeOnly: true }),
+  writeOnlyFalse: joi.string().meta({ writeOnly: false })
 }).id('person').shared(unifiedString)
