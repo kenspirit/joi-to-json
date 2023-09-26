bc92ebc83a889bc80a223c2f83185024fac92380
Ken Chen <ken.chen@cargosmart.com>
2023-09-26 13:30:06 +0800

    Adds option `includeSchemaDialect` to enable including `$schema` in schema output. (Fix #46)

---
+++ test/unit/schema_dialect.spec.js
@@ -0,0 +1,35 @@
+const joi = require('joi-17')
+const parse = require('../../index')
+
+const JOI_SCHEMA = joi.object().keys({
+  field: joi.string()
+})
+
+test('json - includes schema dialect', () => {
+  expect(parse(JOI_SCHEMA, 'json', {}, { includeSchemaDialect: true }).$schema).toEqual('http://json-schema.org/draft-07/schema#')
+})
+
+test('json - no schema dialect', () => {
+  expect(parse(JOI_SCHEMA, 'json').$schema).toEqual(undefined)
+})
+
+test('json-draft-04 - includes schema dialect', () => {
+  expect(parse(JOI_SCHEMA, 'json-draft-04', {}, { includeSchemaDialect: true }).$schema).toEqual('http://json-schema.org/draft-04/schema#')
+})
+
+test('json-draft-04 - no schema dialect', () => {
+  expect(parse(JOI_SCHEMA, 'json-draft-04').$schema).toEqual(undefined)
+})
+
+test('json-draft-2019-09 - includes schema dialect', () => {
+  expect(parse(JOI_SCHEMA, 'json-draft-2019-09', {}, { includeSchemaDialect: true }).$schema).toEqual('https://json-schema.org/draft/2019-09/schema')
+})
+
+test('json-draft-2019-09 - no schema dialect', () => {
+  expect(parse(JOI_SCHEMA, 'json-draft-2019-09').$schema).toEqual(undefined)
+})
+
+test('open-api - no schema dialect even specified', () => {
+  expect(parse(JOI_SCHEMA, 'open-api', {}, { includeSchemaDialect: true }).$schema).toEqual(undefined)
+})
+
